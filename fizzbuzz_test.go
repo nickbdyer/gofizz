860150ab2f3bfd4396e290e9aa3f8408e38df322
Nick Dyer <nickbdyer@users.noreply.github.com>
2018-10-23 17:18:54 +0100

    Import testify library for better assertions

--- fizzbuzz_test.go
+++ fizzbuzz_test.go
@@ -1,48 +1,42 @@
 package main
 
-import "testing"
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
 
 func TestFizzbuzzReturnNumberIfNotDivisible(t *testing.T) {
 	actual := Fizzbuzz(7)
 	expected := "7"
 
-	if actual != expected {
-		t.Errorf("actual '%s' expected '%s'", actual, expected)
-	}
+	assert.Equal(t, expected, actual)
 }
 
 func TestFizzbuzzKnowsIfANumberIsDivisibleByThree(t *testing.T) {
 	actual := Fizzbuzz(3)
 	expected := "Fizz"
 
-	if actual != expected {
-		t.Errorf("actual '%s' expected '%s'", actual, expected)
-	}
+	assert.Equal(t, expected, actual)
 }
 
 func TestFizzbuzzKnowsIfANumberIsNotDivisibleByThree(t *testing.T) {
 	actual := Fizzbuzz(4)
 	expected := "4"
 
-	if actual != expected {
-		t.Errorf("actual '%s' expected '%s'", actual, expected)
-	}
+	assert.Equal(t, expected, actual)
 }
 
 func TestFizzbuzzKnowsIfANumberIsDivisibleByFive(t *testing.T) {
 	actual := Fizzbuzz(5)
 	expected := "Buzz"
 
-	if actual != expected {
-		t.Errorf("actual '%s' expected '%s'", actual, expected)
-	}
+	assert.Equal(t, expected, actual)
 }
 
 func TestFizzbuzzKnowsIfANumberIsDivisibleByFifteen(t *testing.T) {
 	actual := Fizzbuzz(15)
 	expected := "Fizzbuzz"
 
-	if actual != expected {
-		t.Errorf("actual '%s' expected '%s'", actual, expected)
-	}
+	assert.Equal(t, expected, actual)
 }
